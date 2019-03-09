ff0e198087f03832e983ac132bf2a45923e0dcf1
Beatriz Martins <mariabeatriz.dongamartins@gmail.com>
2019-03-09 22:14:01 +0000

    input - no structures

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int i, N, M;
+    int v1, v2;
+
+    scanf("%d", &N);/*number of routers*/ 
+    scanf("%d", &M); /*number of connections*/
+
+    for(i = 0; i < M; i++){
+        scanf("%d %d", &v1, &v2);
+        /*guardar na estrutura*/
+    }
+}
