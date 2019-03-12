9ae9e7aea4676a789987aa3ba0da36fdd70d0a5c
Beatriz Martins <mariabeatriz.dongamartins@gmail.com>
2019-03-12 00:41:29 +0000

    structures, step 1 and 2 done

--- proj.c
+++ proj.c
@@ -5,7 +5,7 @@
 
 typedef struct vertex {
     int id;
-    int color;
+    int colour;
     int predecessor;
     int timeOpen;
     int timeClose;
@@ -22,6 +22,88 @@ int ** getMatrix(Graph* g){
 
 int getSize(Graph* g){
     return g->size;
+}
+
+void printAdj(int * vector, int size){
+    int i,j;
+    for(i = 0; i < size; i++){
+        printf("%d ", vector[i]);
+    }
+}
+
+int * getAdj(int line, Graph* g){
+    int i;
+    int n = 0;
+    int size = getSize(g);
+    int *vectorAdj = (int*)malloc(sizeof (int) * size);
+
+    for(i=0;i<size; i++){
+        if(getMatrix(g)[line-1][i] == 1){
+            vectorAdj[n++] = i + 1;
+        }
+    }
+    return vectorAdj;
+}
+
+Vertex** initialTable(Graph* g){
+	int size = getSize(g);
+	Vertex** table = (Vertex**)malloc(size*sizeof(Vertex*)); 
+	for(int i = 0; i < size; i++){
+        table[i] = (Vertex*)malloc(size*sizeof(Vertex*));
+		table[i]->id = i + 1;
+		table[i]->colour = 0;
+	}
+	return table;
+}
+
+void DfsVisit(Vertex* v, Vertex** table, int *time, int *maxSubgraph, Graph* g){
+    int i = 0;
+    v->colour = 1;
+    v->timeOpen = *time;
+    (*time)++;
+    int* adj = getAdj(v->id, g);
+    int size = getSize(g);
+    if(v->id > (*maxSubgraph)){
+        *maxSubgraph = v->id;
+    }
+    while(adj[i] != 0 || i > size){
+        if(table[adj[i] - 1]->colour == 0){ 
+            table[adj[i] - 1]->predecessor = v->id;
+            DfsVisit(table[adj[i]-1], table, time, maxSubgraph, g);
+        }
+        i++;
+    }
+    v->colour = 2;
+    v->timeClose = *time;
+    (*time)++;
+
+}
+
+void printTable(Vertex **table, int N){
+    int i;
+    for(i = 0; i < N; i++){
+        printf("id: %d;  %d;  %d %d\n", table[i]->id, table[i]->predecessor, table[i]->timeOpen, table[i]->timeClose);
+    }
+}
+
+void Dfs(Graph* g, int N){
+    int i, maxSubgraph;
+    int n = 0;
+    int subgraphs = 0;
+    int *vectorMax = (int*)malloc(sizeof (int) * N);
+    Vertex **table = initialTable(g);
+    int time = 1;
+    for(i = 0; i < N; i++){
+        if(table[i]->colour == 0){
+            maxSubgraph = 0;
+            subgraphs++;
+            DfsVisit(table[i], table, &time, &maxSubgraph, g);
+            vectorMax[n++] = maxSubgraph; /*meter no vetor o valor*/
+        }
+    }
+    printf("VALOR DO M: %d\n", subgraphs);
+    printTable(table, N);
+    printAdj(vectorMax, subgraphs);
 }
 
 void printMatrix(int ** matrix, int size){
@@ -56,28 +138,6 @@ Graph * initialGraph(int N){
     return g;
 }
 
-void printAdj(int * vector, int size){
-    int i,j;
-    for(i = 0; i < size; i++){
-        printf("%d ", vector[i]);
-    }
-}
-
-
-int * getAdj(int line, Graph* g){
-    int i;
-    int n = 0;
-    int size = getSize(g);
-    int *vectorAdj = (int*)malloc(sizeof (int) * size);
-
-    for(i=0;i<size; i++){
-        if(getMatrix(g)[line-1][i] == 1){
-            vectorAdj[n++] = i + 1;
-        }
-    }
-    return vectorAdj;
-}
-
 void addValue(int line, int column, int ** matrix){
     matrix[line-1][column-1] = 1;
     matrix[column-1][line-1] = 1;
@@ -110,7 +170,8 @@ int main(int argc, char **argv) {
     }
 
     /*printMatrix(matrix, N);*/
-    printAdj(getAdj(1, g), N);
+    /*printAdj(getAdj(1, g), N);*/
+    Dfs(g, N);
 }
 
 
